351ba1f145d354c0e5d651582c09eb6bb7b0629b
jospi96 <113528331+jospi96@users.noreply.github.com>
2022-10-28 15:37:49 +0200

    Add files via upload

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import _get require( "lodash/get");
+import _get from "lodash/get";
 
 const app = document.querySelector(".app");
 const form = document.querySelector(".textArea");
